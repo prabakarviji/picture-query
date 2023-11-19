50e207af5bc983fa93aba8005a40180c1732528a
prabakarviji <prabakarviji@gmail.com>
2023-11-19 23:24:38 +0530

    Feat: Pre-defined actions are added

--- Mahizh/ChatView.swift
+++ Mahizh/ChatView.swift
@@ -16,9 +16,17 @@ struct ChatView: View {
                         .foregroundColor(message.sender == .me ? .white : nil)
                         .padding()
                         .background(message.sender == .me ? .black : .gray.opacity(0.4))
-                        .cornerRadius(24)
+                        .cornerRadius(15)
                     if message.sender == .chatGPT{Spacer()}
             }
         }
            
+}
+
+struct ChatViewPreview:PreviewProvider{
+    
+    static var previews: some View{
+        let myMessage = ChatMessage(id: UUID().uuidString, content: "Create a reminder", createdAt: Date(), sender: .me)
+        ChatView(message: myMessage)
+    }
 }
--- Mahizh/ContentView.swift
+++ Mahizh/ContentView.swift
@@ -11,86 +11,111 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @State var chatMessages: [ChatMessage] = []
-    @State var message: String = ""
-    var isLoading: Bool = false
-    
-    @State var lastMessageID: String = ""
-    
-    ///- Dark mode/Light mode variable
-    @Environment(\.colorScheme) var colorScheme
-    
-    @State var cancellables = Set<AnyCancellable>()
-    
-    var body: some View {
-        VStack {
-            HStack {
-                Text("mahizh!")
-                    .font(.title)
-                    .fontWeight(.bold)
-                Spacer()
-            }
-            
-            ScrollViewReader { proxy in
-                ScrollView(.vertical, showsIndicators: false) {
-                    LazyVStack {
-                        ForEach(chatMessages, id: \.id) { message in
-                            ChatView(message: message)
-                        }
-                    }
-                }
-                .onChange(of: self.lastMessageID) { id in
-                    withAnimation{
-                        proxy.scrollTo(id, anchor: .bottom)
-                    }
-                }
-            }
-            
-            HStack {
-                TextField("Message", text: $message) {}
-                    .padding()
-                    .background(colorScheme == .dark ? .gray.opacity(0.2) : .gray.opacity(0.1))
-                    .cornerRadius(10)
-                Button{
-                    sendMessage()
-                } label: {
-                    Image(systemName: "arrow.right.circle.fill")
-                        .foregroundColor(.black)
-                        .padding(.horizontal, 1)
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        
-                }
-            }
-        }
-        .padding()
-    }
-        
-    func sendMessage (){
-        guard message != "" else {return}
-        
-        let myMessage = ChatMessage(id: UUID().uuidString, content: message, createdAt: Date(), sender: .me)
-        chatMessages.append(myMessage)
-        lastMessageID = myMessage.id
-        
-//        openAIService.sendMessage(message: message).sink { completion in
-//            /// - Handle Error here
-//        } receiveValue: { response in
-//            guard let textResponse = response.choices.first?.text.trimmingCharacters(in: .whitespacesAndNewlines.union(.init(charactersIn: "\""))) else {return}
-//            let chatGPTMessage = ChatMessage(id: response.id, content: textResponse, createdAt: Date(), sender: .chatGPT)
-//            
-//            chatMessages.append(chatGPTMessage)
-//            lastMessageID = chatGPTMessage.id
-//        }
-//        .store(in: &cancellables)
-            
-        message = ""
-    }
+	@State var chatMessages: [ChatMessage] = []
+	@State var message: String = ""
+	var isLoading: Bool = false
+	var options: [String] = ["Create a reminder","Add a task","Capture a picture"]
+	
+	@State var lastMessageID: String = ""
+	
+	///- Dark mode/Light mode variable
+	@Environment(\.colorScheme) var colorScheme
+	
+	@State var cancellables = Set<AnyCancellable>()
+	
+	var body: some View {
+		VStack {
+			HStack {
+				Text("mahizh!")
+					.font(.title)
+					.fontWeight(.bold)
+				Spacer()
+			}
+			ScrollViewReader { proxy in
+				ScrollView(.vertical, showsIndicators: false) {
+					LazyVStack {
+						ForEach(chatMessages, id: \.id) { message in
+							ChatView(message: message)
+						}
+					}
+				}
+				.onChange(of: self.lastMessageID) { id in
+					withAnimation{
+						proxy.scrollTo(id, anchor: .bottom)
+					}
+				}
+			}
+			ScrollView(.horizontal) {
+				LazyHStack {
+					
+					ForEach(options,id:\.self){option in
+						Button(action:{
+							setMessage(selectedOption: option)
+						}) {
+							Text(option)
+								.padding()
+								.background(.gray)
+								.opacity(0.5)
+								.cornerRadius(10)
+								.foregroundColor(.black)
+						}
+					}
+				}
+				.padding(0)
+			}
+			.frame(height: 80)
+			.background(Color(.white))
+			Spacer().frame(height: 5)
+			HStack {
+				TextField("Message", text: $message) {}
+					.padding()
+					.background(colorScheme == .dark ? .gray.opacity(0.2) : .gray.opacity(0.1))
+					.cornerRadius(10)
+				Button{
+					sendMessage()
+				} label: {
+					Image(systemName: "arrow.right.circle.fill")
+						.foregroundColor(.black)
+						.padding(.horizontal, 1)
+						.font(.largeTitle)
+						.fontWeight(.semibold)
+					
+				}
+			}
+		}
+		.padding()
+	}
+	
+	func setMessage(selectedOption:String){
+		message = selectedOption
+		sendMessage()
+	}
+	
+	func sendMessage (){
+		guard message != "" else {return}
+		
+		let myMessage = ChatMessage(id: UUID().uuidString, content: message, createdAt: Date(), sender: .me)
+		chatMessages.append(myMessage)
+		lastMessageID = myMessage.id
+		
+		//        openAIService.sendMessage(message: message).sink { completion in
+		//            /// - Handle Error here
+		//        } receiveValue: { response in
+		//            guard let textResponse = response.choices.first?.text.trimmingCharacters(in: .whitespacesAndNewlines.union(.init(charactersIn: "\""))) else {return}
+		//            let chatGPTMessage = ChatMessage(id: response.id, content: textResponse, createdAt: Date(), sender: .chatGPT)
+		//
+		//            chatMessages.append(chatGPTMessage)
+		//            lastMessageID = chatGPTMessage.id
+		//        }
+		//        .store(in: &cancellables)
+		
+		message = ""
+	}
 }
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
